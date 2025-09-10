c2c2a945753c63fc558faade7d2180f38a5c7a24
zanthanh17 <vanthanh1752k3@gmail.com>
2025-09-10 14:44:39 +0700

    nothing

--- humoments.py
+++ humoments.py
@@ -24,14 +24,14 @@ def compute_hu_moments(image_path):
     return hu_log
 
 # Thư mục chứa ảnh
-folder_path = "./"  # nếu ảnh nằm cùng folder với script
-output_csv = "hu_features.csv"
+folder_path = "./binary_images"  # nếu ảnh nằm cùng folder với script
+output_csv = "./data/hu_features.csv"
 
 # Lưu kết quả
 with open(output_csv, mode='w', newline='') as f:
     writer = csv.writer(f)
     # Ghi header
-    header = ["Image", "Hu1", "Hu2", "Hu3", "Hu4", "Hu5", "Hu6", "Hu7"]
+    header = ["Image", "Hu1", "Hu2", "Hu3", "Hu4", "Hu5", "Hu6", "Hu7", "label"]
     writer.writerow(header)
 
     # Lặp qua 50 ảnh
@@ -41,7 +41,9 @@ with open(output_csv, mode='w', newline='') as f:
         try:
             hu_features = compute_hu_moments(image_path)
             # Ghi vào CSV
-            writer.writerow([image_name] + hu_features.tolist())
+            # Làm tròn 2 số sau dấu phẩy
+            rounded_features = [round(x, 4) for x in hu_features]
+            writer.writerow([image_name] + rounded_features)
             print(f"Đã xử lý: {image_name}")
         except Exception as e:
             print(f"Lỗi với {image_name}: {e}")
